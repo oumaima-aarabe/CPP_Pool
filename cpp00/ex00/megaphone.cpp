f27dba3a23a2c63bced3f619905a0f3dfd2ef97c
Oumaima Aarabe <ouaarabe@e3r10p17.1337.ma>
2023-11-20 07:15:45 +0100

    meg

--- cpp00/ex00/megaphone.cpp
+++ cpp00/ex00/megaphone.cpp
@@ -6,7 +6,7 @@
 /*   By: ouaarabe <ouaarabe@student.1337.ma>        +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/08/22 19:44:26 by ouaarabe          #+#    #+#             */
-/*   Updated: 2023/08/22 22:50:15 by ouaarabe         ###   ########.fr       */
+/*   Updated: 2023/11/20 07:14:13 by ouaarabe         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -15,8 +15,11 @@
 int main(int ac, char **av)
 {
     if (ac == 2)
-        for (int i = 0; av[1][i]; i++)
-            std::cout << (char)toupper(av[1][i]);
+    {
+        std::string str = av[1];   
+        for (int i = 0; str[i]; i++)
+            std::cout << (char)toupper(str[i]);
+    }
     else
         std::cout << "* LOUD AND UNBEARABLE FEEDBACK NOISE *";
     std::cout << "\n";

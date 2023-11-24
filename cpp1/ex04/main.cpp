63dca13e548e2ff6a5241acfb3ded0741674cd96
oumiiii <oaarabe2307@gmail.com>
2023-11-24 11:23:00 +0100

    sed

--- cpp1/ex04/main.cpp
+++ cpp1/ex04/main.cpp
@@ -1,43 +1,60 @@
 #include <iostream>
 #include <fstream>
+#include <string>
 
-int main(int ac , char **av)
-{
-	if (ac != 4)
-		return (std::cerr << " <filename>  s1  s2 \n", 1);
-	std::ifstream infile;
-	std::string s1 = av[2];
-	std::string s2 = av[3];
-	infile.open(av[1]);
-	if (!infile)
-		return(std::cerr << "Error open " << av[1] << std::endl , 1);
-	std::ofstream outfile((std::string(av[1]) + "_s1ace").c_str());
-	if (!outfile)
-		return (std::cerr << "Error while creating file\n",  1);
-	std::string data;
-	std::getline(infile, data, '\0');
-	if(s1 == "")
+int main(int ac, char **av) {
+    if (ac != 4) {
+        std::cerr << "Usage: " << av[0] << " <filename> <s1> <s2>\n";
+        return 1;
+    }
+
+    std::ifstream inputfile(av[1]);
+    if (!inputfile.is_open()) {
+        std::cerr << "Error opening file: " << av[1] << std::endl;
+        return 1;
+    }
+
+	if (inputfile.peek() == std::ifstream::traits_type::eof()) 
 	{
-		outfile << data;
-		outfile.close();
-		infile.close();
-		return (0);
-	}
-	int i =  data.find(s1);
-	while(i != -1)
+		std::cerr << "Error: Input file is empty" << std::endl;
+        inputfile.close();
+        return 1;
+    }
+    std::string s1 = av[2];
+    std::string s2 = av[3];
+    std::string o_filename = std::string(av[1]) + ".replace";
+    std::ofstream outputfile(o_filename.c_str());
+
+    if (!outputfile.is_open()) {
+        std::cerr << "Error creating output file\n";
+        inputfile.close();
+        return 1;
+    }
+
+    std::string line;
+    while (std::getline(inputfile, line)) 
 	{
-		if(i == 0)
-			data =  s2 + data.substr(i + s1.size(), data.size() - s1.size());
-		else
+        size_t pos = 0;
+		if (!s1.empty())
 		{
-			if(i + data.size() == '\0')
-				data = data.substr(0, i) + s2;
-			else
-				data = data.substr(0, i) + s2 + data.substr(i + s1.size());
+			while ((pos = line.find(s1, pos)) != std::string::npos) 
+			{
+				if (!s2.empty())
+				{
+					line = line.substr(0, pos) + s2 + line.substr(pos + s1.length());
+					pos += s2.length();
+				}
+				else
+					line = line.substr(0, pos) + line.substr(pos + s1.length());
+			}
 		}
-		i =  data.find(s1 , i + s2.length());
-	}
-	outfile << data;
-	infile.close();
-	outfile.close();
+        outputfile << line << std::endl;
+    }
+
+    std::cout << "Replacement completed. Output written to: " << o_filename << std::endl;
+
+    inputfile.close();
+    outputfile.close();
+
+    return 0;
 }

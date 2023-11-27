b830fa5cbe19fd1075844951beb216902db4980b
Oumaima Aarabe <ouaarabe@e3r10p14.1337.ma>
2023-11-27 15:32:46 +0100

    relink

--- cpp1/ex05/Harl.cpp
+++ cpp1/ex05/Harl.cpp
@@ -1,19 +1,19 @@
 #include "Harl.hpp"
 
 void Harl::debug() {
-    std::cout << "I asked for a cheeseless mac and cheese!!!\n";
+    std::cout << "I asked for a cheeseless mac and cheese!!!" << std::endl;
 }
 
 void Harl::info() {
-    std::cout << "I cannot believe removing cheese does not reduce the fees\n";
+    std::cout << "I cannot believe removing cheese does not reduce the fees" << std::endl;
 }
 
 void Harl::warning() {
-    std::cout << "i think i should not pay for extra ingredients i didnt get\n";
+    std::cout << "i think i should not pay for extra ingredients i didnt get" << std::endl;
 }
 
 void Harl::error() {
-    std::cout << "i am giving a really bad review to this place\n";
+    std::cout << "i am giving a really bad review to this place" << std::endl;
 }
 
 void Harl::complain(std::string level) 

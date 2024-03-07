b48a4203a2a958bb7e3a4f3d458c2ae28b67afdd
Cupcake <cupcake@Stevens-MacBook-Air.local>
2024-03-07 16:36:53 +0100

    chajar

---
+++ cpp05/ex02/ShrubberyCreationForm.cpp
@@ -0,0 +1,61 @@
+#include "ShrubberyCreationForm.hpp"
+#include <cstdlib>
+#include <ctime>
+
+ShrubberyCreationForm::ShrubberyCreationForm()
+{
+
+}
+
+ShrubberyCreationForm::ShrubberyCreationForm(std::string target) : AForm(target, 145, 137)
+{
+
+}
+
+ShrubberyCreationForm::~ShrubberyCreationForm()
+{
+
+}
+
+ShrubberyCreationForm::ShrubberyCreationForm(const ShrubberyCreationForm& scf) : AForm(scf)
+{
+    *this = scf;
+}
+
+ShrubberyCreationForm& ShrubberyCreationForm::operator=(const ShrubberyCreationForm &scf)
+{
+    if (this != &scf)
+        AForm::operator=(scf);
+    return (*this);
+}
+
+void ShrubberyCreationForm::execute(const Bureaucrat& executor) const
+{
+    if (!this->getIfSigned())
+    {
+        std::cerr << executor.getName() << " can't execute scf, form is unsigned" << std::endl;
+        return;
+    }
+    else if(this->getGrade() < executor.getGrade())
+        throw AForm::GradeTooLowException();
+    std::ofstream out(executor.getName() + _shrubbery.c_str());
+
+    if(!out.isopen())
+        return(std::cerr << "error opening file" << std::endl);
+    
+
+    out << "              v .   ._, |_  .,\n"
+        << "           `-._\/  .  \ /    |/_\n"
+        << "               \\  _\, y | \//\n"
+        << "         _\_.___\\, \\/ -.\||\n"
+        << "           `7-,--.`._||  / / ,\n"
+        << "           /'     `-. `./ / |/_.'\n"
+        << "                     |    |//\n"
+        << "                     |_    /\n"
+        << "                     |-   |\n"
+        << "                     |   =|\n"
+        << "                     |    |\n"
+        << "--------------------/ ,  . \--------._\n"
+    
+
+}
